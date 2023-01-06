e6a70cf6802444252c333f37beb22f6f3eda4c89
Prabhat9129 <117995361+Prabhat9129@users.noreply.github.com>
2023-01-06 17:18:47 +0530

    Update file1.php

--- file1.php
+++ file1.php
@@ -1,7 +1,7 @@
 <?php  
 this is php fkkile add inti main2 branch for practice
-this istfugjkjkhju a;'l'lsdhdhdhfhnnco seprkhhkabhatcond change from main branch not master branch
-this helkiuikjkkis secondrrf hellochange hello12233455  from master1 branch
+this istfugjkjkhju a;'l'lsdhdhdhfhnnco sl;l;;hgeprkhhkabhatcond change from main branch not master branch
+this helkiuikjkkis klkkllk secondrrf hellochange hello12233455  from master1 branch
 and solve the co'
 ;'
   nflict here
@@ -9,7 +9,7 @@ and solve the co'
   hello change 
 yiggih hell bbo 
 fromgtf new bruihjjhiuanch
-  
+  elkklkli change 
   hello change is herere jddhdhdhsdhdsh
 change isj yhuiibshbsgdshgfduhere
   gyii
