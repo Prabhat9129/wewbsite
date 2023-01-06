9e6ea446665d7c320c8d957c55b6b9f6faaa1a93
Prabhat Dixit <2002dixtprabhat@gmail.com>
2023-01-06 16:56:24 +0530

    hello mybranch

--- file1.php
+++ file1.php
@@ -1,7 +1,7 @@
 <?php  
 this is php file add inti main2 branch for practice
 this istfugu also seprabhatcond change from main branch not master branch
-this helkiuikkis second hellochange from master1 branch
+this helkiuikkis second hellochange hello  from master1 branch
 and solve the conflict here
   mjmjmjm
 yiggih hello 
