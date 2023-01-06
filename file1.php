a7f6450068bde56de39f774a7b519583d714700e
Prabhat Dixit <2002dixtprabhat@gmail.com>
2023-01-06 16:43:59 +0530

    from master branch

--- file1.php
+++ file1.php
@@ -3,5 +3,7 @@ this is php file add inti main2 branch for practice
 this is also second change from main branch not master branch
 this helkkkis second hellochange from master branch
 and solve the conflict here
+
+from new branch
 change is here
 ?>
