12bff6c2cdb7dd40d7a4bf36bae13a0fbca9bbd7
Prabhat9129 <117995361+Prabhat9129@users.noreply.github.com>
2023-01-06 17:11:00 +0530

    Update file1.php

--- file1.php
+++ file1.php
@@ -1,11 +1,12 @@
 <?php  
 this is php fkkile add inti main2 branch for practice
-this istfugjkjkhju a;'l'lso seprkhhkabhatcond change from main branch not master branch
+this istfugjkjkhju a;'l'lsdhdhdhfhnnco seprkhhkabhatcond change from main branch not master branch
 this helkiuikjkkis secondrrf hellochange hello12233455  from master1 branch
 and solve the co'
 ;'
   nflict here
   mjmjmjmskjkjljhggs
+  hello change 
 yiggih hell bbo 
 fromgtf new bruihjjhiuanch
 change isj yhuiibshbsgdshgfduhere
