cd0df83d73222cdbac39edfb909d9b3141935083
Prabhat9129 <117995361+Prabhat9129@users.noreply.github.com>
2023-01-06 17:09:55 +0530

    Update file1.php

--- file1.php
+++ file1.php
@@ -1,11 +1,13 @@
 <?php  
 this is php fkkile add inti main2 branch for practice
-this istfugjkjkhju also seprkhhkabhatcond change from main branch not master branch
+this istfugjkjkhju a;'l'lso seprkhhkabhatcond change from main branch not master branch
 this helkiuikjkkis secondrrf hellochange hello12233455  from master1 branch
-and solve the conflict here
-  mjmjmjmskjhggs
+and solve the co'
+;'
+  nflict here
+  mjmjmjmskjkjljhggs
 yiggih hell bbo 
-fromgtf new bruiiuanch
-change is yhuiibshbsgdshgfduhere
+fromgtf new bruihjjhiuanch
+change isj yhuiibshbsgdshgfduhere
   gyii
 ?>
